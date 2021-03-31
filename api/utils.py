e2546f754547512a00a66def3f7300cab0fb2054
jsc <jimdevops@gmail.com>
2021-03-31 12:36:47 +0300

    Adding user report to request in queue

--- api/utils.py
+++ api/utils.py
@@ -13,8 +13,20 @@ def get_auth_token_header(request):
 
 
 def token_exists(token_to_validate):
+    '''
+    Returns if the token exists or not
+    :param token_to_validate:
+    :return: bool
+    '''
     try:
         User.objects.get(auth_token=token_to_validate)
         return True
     except:
         return False
+
+def get_user_object_by_token(request):
+    # Token Header tuple, we should grab the provided token string
+    key, token_string = get_auth_token_header(request)
+    token = token_string.replace('Token ', '')
+    user_obj = User.objects.get(auth_token=token)
+    return user_obj
