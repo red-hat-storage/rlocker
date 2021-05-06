69bee4daec6836d49c9b7ab0a3f5fbdfbf634125
jimdevops19 <jimdevops@gmail.com>
2021-05-06 11:17:21 +0300

    Adding healthcheck

--- health/views.py
+++ health/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+@api_view(['GET'])
+def health_check(request):
+    return Response('OK')
