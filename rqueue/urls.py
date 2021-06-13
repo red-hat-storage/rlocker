c082b742aef648262986ea440b97851f8378530c
jimdevops19 <jimdevops@gmail.com>
2021-06-13 10:17:46 +0300

    several important changes

--- rqueue/urls.py
+++ rqueue/urls.py
@@ -4,4 +4,6 @@ from rqueue import views
 urlpatterns = [
     path('pendingrequests/', views.pending_requests_page, name='pending_requests_page'),
     path('finishedrequests/', views.finished_requests_page, name='finished_requests_page'),
+    path('rqueues/<slug>', views.rqueue_more_info, name='rqueue_more_info'),
+
 ]
