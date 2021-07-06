f2e21a5d58f86d143545b26cf3a9542f14bf990e
Cem Erginbas <cerginba@cerginba.tlv.csb>
2021-07-06 08:25:41 +0300

    Final version for create non existing users script

--- scripts/create_non_existing_users.py
+++ scripts/create_non_existing_users.py
@@ -1,10 +1,10 @@
-from django.contrib.auth.models import Permission, User
+from django.contrib.auth.models import User, Group
 
 
 def run(
         list_of_users=None,
         initial_password=None,
-        list_of_perms=None
+        list_of_groups=None
 ):
     """
     Create all the users provided in the list
@@ -18,23 +18,32 @@ def run(
     if not initial_password:
         initial_password = input("Decide initial password for all the users, they can change it in the first login")
 
-    if not list_of_perms:
-        list_of_perms = input("List of Permissions ? Comma Separated (Without spacing)")
-        list_of_perms = list_of_perms.split(',')
+    if not list_of_groups:
+        list_of_groups = input("List of Groups ? Comma Separated (Without spacing) \n"
+                               f"List of all groups: \n"
+                               f"{Group.objects.all()}")
+        list_of_groups = list_of_groups.split(',') if list_of_groups != '' else []
 
     for user in list_of_users:
         try:
-            User.objects.get(username=user)
+            existing_user = User.objects.get(username=user)
             print(f"{user} exists! skipping ...")
+            if len(list_of_groups) > 0:
+                for group in list_of_groups:
+                    group = Group.objects.get(name=group)
+                    group.user_set.add(existing_user)
+                    print(f"User added to group -> {group}")
+
         except User.DoesNotExist:
             # If we are here, it means that the User objects does not exist, let's create it
             create_attempt = User.objects.create_user(
                 user, password=initial_password
             )
-            for perm in list_of_perms:
-                perm_object = Permission.objects.get(name=perm)
-                create_attempt.user_permissions.add(perm_object)
-
+            if len(list_of_groups) > 0:
+                for group in list_of_groups:
+                    group = Group.objects.get(name=group)
+                    group.user_set.add(create_attempt)
+                    print(f"User added to group -> {group}")
             create_attempt.save()
             print(f"User {user} created successfully!")
 
