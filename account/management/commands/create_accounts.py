696e8796ea1e4ab31fd5467424c6992f51255b60
jimdevops19 <jimdevops@gmail.com>
2021-07-14 09:56:59 +0300

    Change Django scripts to management commands for simple execution

---
+++ account/management/commands/create_accounts.py
@@ -0,0 +1,47 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User, Group
+
+
+class Command(BaseCommand):
+    help = "Command for creating accounts that are not existing on the platform"
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        """
+        Create all the users provided in the list
+
+        :return: None
+        """
+
+        list_of_users = input("List of Users ? Comma Separated (Without spacing)\n")
+        list_of_users = list_of_users.split(',')
+        initial_password = input("Decide initial password for all the users, they can change it in the first login\n")
+        list_of_groups = input("List of Groups ? Comma Separated (Without spacing) \n"
+                               f"List of all groups: \n"
+                               f"{Group.objects.all()} \n")
+        list_of_groups = list_of_groups.split(',') if list_of_groups != '' else []
+
+        for user in list_of_users:
+            try:
+                existing_user = User.objects.get(username=user)
+                print(f"{user} exists! skipping ...")
+                if len(list_of_groups) > 0:
+                    for group in list_of_groups:
+                        group = Group.objects.get(name=group)
+                        group.user_set.add(existing_user)
+                        print(f"User added to group -> {group}")
+
+            except User.DoesNotExist:
+                # If we are here, it means that the User objects does not exist, let's create it
+                create_attempt = User.objects.create_user(
+                    user, password=initial_password
+                )
+                if len(list_of_groups) > 0:
+                    for group in list_of_groups:
+                        group = Group.objects.get(name=group)
+                        group.user_set.add(create_attempt)
+                        print(f"User added to group -> {group}")
+                create_attempt.save()
+                print(f"User {user} created successfully!")
---
+++ lockable_resource/management/commands/release_all.py
@@ -0,0 +1,24 @@
+from django.core.management.base import BaseCommand
+from lockable_resource.models import LockableResource
+
+class Command(BaseCommand):
+    help = "Command for Releasing all resources"
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        """
+            Release all lockable resources
+        :return: None
+        """
+        confirmation = input("Confirm ? "
+                             "This will release all the locked resources. Y/N")
+        if confirmation.lower() == 'y':
+            for lr in LockableResource.objects.all():
+                try:
+                    lr.release()
+                except:
+                    print(f"Skipping {lr.name}. It is released ...")
+
+            print("All Lockable resources have been released!")
