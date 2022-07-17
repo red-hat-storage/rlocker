facb31d22ae5e2d4a20778c06fa4685bf0189b80
JimSC <49904449+jimdevops19@users.noreply.github.com>
2022-07-17 15:35:27 +0300

    Fix import of settings module (#33)

--- rlocker/urls.py
+++ rlocker/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
