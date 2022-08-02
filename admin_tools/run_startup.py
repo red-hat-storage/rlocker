4cd1c192353ca68cba2420754ad0524e9b7b3106
Jim Erg <jimdevops@gmail.com>
2022-08-02 15:39:11 +0300

    Insert supported addons only on runserver and not in migration commands

--- admin_tools/run_startup.py
+++ admin_tools/run_startup.py
@@ -2,7 +2,19 @@
 # is being recognized as an INSTALLED_APP (INSTALLED_ADDON)
 # Use this file to add code that should be executed first time
 
+import sys
 from admin_tools.models import Addon
 
+def insert_supported_addons_on_runserver():
+    """
+    Run this chunk of command only on runserver
+    """
+    sub_command = sys.argv[2]
+    if sub_command == "runserver":
+        Addon.insert_supported_addons()
+    else:
+        pass
+
+
 def main():
-    Addon.insert_supported_addons()
+    insert_supported_addons_on_runserver()
